afee58765947a8730dbd38cc22cb124f951f415d
madhu <madhu@192.168.1.57>
2025-10-01 12:08:03 +0530

    updated hello world

--- src/demo/HelloWorld.java
+++ src/demo/HelloWorld.java
@@ -2,6 +2,6 @@ package demo;
 
 public class HelloWorld {
 	public static void main(String args[]) {
-		System.out.println("hello world");
+		System.out.println("hello world and the text updated");
 	}
 }
